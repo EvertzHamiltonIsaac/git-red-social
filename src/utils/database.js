ddb0e038f4d803cd348b1462a0de8607968fe506
EvertzHamiltonIsaac <hamiltonevertz99@gmail.com>
2022-12-21 22:46:52 -0400

    Resolviste el problema que habia con la variable de entorno NODE_ENV

--- src/utils/database.js
+++ src/utils/database.js
@@ -7,8 +7,18 @@ const db = new Sequelize({
     username: config.db.user,
     password: config.db.pass,
     database: config.db.name,
-    port: config.db.port
+    port: config.db.port,
+    //! Esto se utiliza para produccion, Dara error si no pones esta configuracion.
+    //! Todas las plataformas como servicio tiene esa variable de entorno configurada.
+    dialectOptions: process.env.NODE_ENV === 'production' 
+    ? {
+    
+    ssl: {
+        require: true,
+        rejectUnauthorized: false
+    }
+    
+    } : {}
 })
-
 
 module.exports = db
